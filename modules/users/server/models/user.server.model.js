f1876b5caf0c266d67b3c1461bb41f47d96b529f
Falkirks <me@falkirks.com>
2016-03-02 21:05:07 -0800

    Disable enforced password strength

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -121,13 +121,13 @@ UserSchema.pre('save', function (next) {
  * Hook a pre validate method to test the local password
  */
 UserSchema.pre('validate', function (next) {
-  if (this.provider === 'local' && this.password && this.isModified('password')) {
+  /*if (this.provider === 'local' && this.password && this.isModified('password')) {
     var result = owasp.test(this.password);
     if (result.errors.length) {
       var error = result.errors.join(' ');
       this.invalidate('password', error);
     }
-  }
+  }*/
 
   next();
 });

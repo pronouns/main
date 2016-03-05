61a600fa2e5d841b335def2f9b5c5b13b0cbf64c
Falkirks <me@falkirks.com>
2016-03-04 18:37:32 -0800

    Updated

--- modules/core/server/routes/core.server.routes.js
+++ modules/core/server/routes/core.server.routes.js
@@ -11,5 +11,5 @@ module.exports = function (app) {
   app.route('/:url(api|modules|lib)/*').get(core.renderNotFound);
 
   // Define application route
-  app.route('/*').get(core.renderIndex);
+  app.route('/*').get(core.renderIndex).post(core.renderIndex);
 };

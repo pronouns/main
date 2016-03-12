f371a482ba2c39b8ceb0dc8a6570e40c0c7bb9bd
Falkirks <me@falkirks.com>
2016-03-12 10:02:32 -0800

    Style updates

--- modules/core/client/controllers/faq.client.controller.js
+++ modules/core/client/controllers/faq.client.controller.js
@@ -14,10 +14,6 @@ angular.module('core').controller('FaqController', ['$scope', 'Authentication', 
       {
         title: 'What if my pronouns don\'t exist in the list yet?',
         content: 'Click on "Create pronouns" and enter your information. Then you can click the "+" icon and add it right away. The pronoun will be reviewed by a staff member later and additional details may appear on the page.'
-      },
-      {
-        title: 'My pronouns are "anything goes" or something similar, how can I add that?',
-        content: 'Go to the "update pronouns" section of your settings page. If you have any pronouns listed, remove them. You can now enter arbitrary text to describe your pronouns. This text will only display if you have no other pronouns added.'
       }
     ];
   }

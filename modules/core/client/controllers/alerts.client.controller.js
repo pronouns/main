9c52f33999e7f4a496a6fc847e8dac201c8e4229
Falkirks <me@falkirks.com>
2016-04-14 18:28:34 -0700

    Removed alerts tests while alerts module is volitile

--- modules/core/client/controllers/alerts.client.controller.js
+++ modules/core/client/controllers/alerts.client.controller.js
@@ -3,7 +3,7 @@
 angular.module('core').controller('AlertsController', ['$scope',
   function ($scope) {
     $scope.alerts = [
-      { type: 'warning', msg: 'Pronouny is running a new alerts module. You may experience issues with alert processing. You can report issues to hello@pronount.xyz or on Github.' }
+      { type: 'warning', msg: 'Pronouny is running a new alerts module. You may experience issues with alert processing. You can report issues to hello@pronouny.xyz or on Github.' }
     ];
     $scope.closeAlert = function (index) {
       $scope.alerts.splice(index, 1);

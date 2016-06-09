3e4be48cb0b24dddca795cf393ac092ef0f59b1c
Falkirks <me@falkirks.com>
2016-06-09 09:51:41 -0700

    Built JS

--- public/dist/application.js
+++ public/dist/application.js
@@ -978,6 +978,9 @@ angular.module('pronouns').controller('CreatePronounController', ['$scope', '$st
 
 angular.module('pronouns').controller('PronounListController', ['$scope', '$filter', 'Users', 'Authentication', 'Pronouns',
   function ($scope, $filter, Users, Authentication, Pronouns) {
+    $scope.authentication = Authentication;
+    $scope.user = Authentication.user;
+
     Pronouns.query(function (data) {
       $scope.pronouns = data;
       $scope.buildPager();
@@ -1322,6 +1325,10 @@ angular.module('users').config(['$stateProvider',
       .state('settings.profile', {
         url: '/profile',
         templateUrl: 'modules/users/client/views/settings/edit-profile.client.view.html'
+      })
+      .state('settings.names', {
+        url: '/names',
+        templateUrl: 'modules/users/client/views/settings/update-names.client.view.html'
       })
       .state('settings.password', {
         url: '/password',
@@ -1935,6 +1942,52 @@ angular.module('users').controller('SettingsController', ['$scope', 'Authenticat
     $scope.user = Authentication.user;
   }
 ]);
+
+'use strict';
+
+angular.module('users').controller('UpdateNamesController', ['$scope', '$http', '$location', 'Users', 'Authentication',
+  function ($scope, $http, $location, Users, Authentication) {
+    $scope.user = Authentication.user;
+
+    $scope.names = $scope.user.names;
+    $scope.newName = '';
+
+    if($scope.user.displayName !== null && $scope.names.length === 0){
+      $scope.names[0] = $scope.user.displayName;
+      $scope.user.displayName = null;
+    }
+
+    $scope.addName = function (){
+      if($scope.newName.length > 0) {
+        $scope.names.push($scope.newName);
+        $scope.updateNames();
+        $scope.newName = '';
+      }
+    };
+
+    $scope.removeName = function(name){
+      $scope.names.splice($scope.names.indexOf(name), 1);
+      $scope.updateNames();
+    };
+
+    // Update a user profile
+    $scope.updateNames = function (isValid) {
+      $scope.user.names = $scope.names;
+
+      var user = new Users($scope.user);
+
+      user.$update(function (response) {
+        $scope.success = true;
+        Authentication.user = response;
+        $scope.user = Authentication.user;
+        $scope.names = $scope.user.names;
+      }, function (response) {
+        $scope.error = response.data.message;
+      });
+    };
+  }
+]);
+
 
 'use strict';
 

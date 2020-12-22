7d721be4766006b9979e7c9e5fd6c2fac6c27f07
n <hi@noa.dev>
2020-12-22 07:29:48 -0800

    added 0.0.0.0 host

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,7 @@ module.exports = {
     privateKey: './config/sslcerts/key.pem',
     certificate: './config/sslcerts/cert.pem'
   },
+  host: '0.0.0.0',
   port: process.env.PORT || 8443,
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
